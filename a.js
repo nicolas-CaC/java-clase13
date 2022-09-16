aaefbe99458d63d4a9e886987e98600bc8fc3fdc
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2022-09-15 21:13:40 -0300

    clase 13 java

---
+++ a.js
@@ -0,0 +1,70 @@
+console.clear();
+// console.log('HOLA CHICOS')
+// console.warn('Soy el warning')
+// console.error('Yo no soy un error real')
+
+// alert('soy una alerta')
+
+// let variable = prompt('Ingresame un número, y te digo el cuadrado de ese número en la consola:')
+
+// let opcion = confirm('¿Acepta que va a ver el cuadrado del número que puso?')
+
+// console.log("El valor de la variable es:", variable)
+
+// if (opcion) {
+//     console.log('El valor del cuadrado de la variable es:', variable ** 2)
+// }
+// else {
+//     console.log("El valor esta vez fue falso, no te voy a mostrar el cuadrado", opcion)
+// }
+
+// console.log('Finalizamos el script')
+// let a = 3 > 1;
+
+// if (5 >= 5) {
+//     console.log('sí, es mayor o igual')
+// } else {
+//     console.log('es menor')
+// }
+
+let a = 10;
+let b = 20;
+let c = true;
+let d = 'a es MENOR a b';
+
+// if (a > b) {
+//     console.log('a es mayor a b')
+// } else {
+//     console.log(d.toLowerCase())
+// }
+
+// a = 10;
+let x = '10';
+// x = Number(x);
+x = 0.1;
+// x AHORA VALE 10 (NUMERICO)
+// x = ' 
+
+// x = true;
+
+x = Boolean(x);
+console.log('EL VALOR DE X CONVERTIDO A BOOLEANO ES', x)
+
+// console.log(typeof x)
+// console.log(typeof x.toString())
+
+
+// if (a === x) {
+//     console.log('A es igual a B')
+// } else {
+//     console.log('A NO ES IGUAL A B')
+// }
+
+// let sol = 'amarillo'
+// let pasto = 'verde'
+
+// if (sol === pasto) {
+//     console.log('el sol tiene el color del pasto')
+// } else {
+//     console.log('el sol no tiene ese color')
+// }
